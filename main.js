550f37594bf7d6cf544d5af51829cc296d97cf4d
CoryStafford <corystaffordmusic@gmail.com>
2022-02-18 15:01:54 -0600

    Calculator exercise

---
+++ main.js
@@ -0,0 +1,54 @@
+/*const add = (num1, num2) => {
+    let sum = num1 + num2 
+        return sum
+}
+let addSum = add(2, 3) 
+    console.log(addSum)
+
+const sub = (num1, num2) => {
+    let sum = num1 - num2 
+        return sum
+}
+let subSum = sub(4, 3)
+console.log(subSum)
+
+const multiply = (num1, num2) => {
+    let sum = num1 * num2 
+        return sum
+    }
+    let multiplySum = multiply(4, 4)
+    console.log(multiplySum)
+
+const divide = (num1, num2) => {
+    let sum = num1 / num2
+        return sum
+    }
+    let divideSum = divide(10, 2)
+    console.log(divideSum)
+
+    const square = (num1) => {
+        const sum = num1 * num1
+        return sum
+    }
+    let squareSum = square(4)
+    console.log(squareSumm)*/
+
+    //A quarter of the animals on a farm have four legs. 
+    //The remaining three quarters of the animals have two legs. 
+    //If there are 60 legs overall, how many animals are there on the farm?
+
+    const twoLegs = (num1, num2) => {
+       sum = num1 * num2
+       return sum
+    } 
+    let twoLegsSum = twoLegs(60, 0.25)   
+    console.log(twoLegsSum) 
+
+    const fourLegs = (num1, num2) => {
+        sum = num1 * num2
+        return sum
+    }
+    let fourLegsSum = fourLegs(60, .75)
+    console.log(fourLegsSum)
+
+    
